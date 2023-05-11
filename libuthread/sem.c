5cbe548c07568fcd7d8929e56893a570c5d7969d
GitYaw <ymir8091@gmail.com>
2023-05-11 01:17:57 -0700

    create semaphore struct

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -6,17 +6,32 @@
 #include "private.h"
 
 struct semaphore {
-	/* TODO Phase 3 */
+	int count;
+	queue_t blockedQueue;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 3 */
+	// Allocate space for semaphore
+	sem_t semaphore = (sem_t) malloc(sizeof(semaphore));
+	// Create blocked queue for threads (waitlist)
+	semaphore->blockedQueue = queue_create();
+	// Initialize count
+	semaphore->count = count;
+
+	return semaphore;
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 3 */
+	if (sem == NULL || queue_length(sem->blockedQueue) != 0) {
+		return -1;
+	}
+
+	queue_destroy(sem->blockedQueue);
+	free(sem);
+
+	return 0;
 }
 
 int sem_down(sem_t sem)
